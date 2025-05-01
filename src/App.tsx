f2304f9cabd1de3480f7c10b2a0c8f99ce5f63d4
NithyaKrishnan08 <56358639+NithyaKrishnan08@users.noreply.github.com>
2025-05-01 10:05:12 -0400

    useActionState hook in React 19

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 // import Theme from "./components/Theme";
 // import Form from "./components/Form";
-import Form2 from "./components/Form2";
+// import Form2 from "./components/Form2";
+import Count from "./components/Count";
 import './index.css'
 
 const App = () => {
   return (
     <div>
-      <Form2 />
+      <Count />
     </div>
   );
 };

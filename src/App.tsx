1c7d02f5694e506b88814cb9a6685bc7f25f192d
NithyaKrishnan08 <56358639+NithyaKrishnan08@users.noreply.github.com>
2025-05-01 09:24:34 -0400

    use hook in React 19

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-// import { useState } from 'react'
+import Theme from "./components/Theme";
+import './index.css'
 
-function App() {
+const App = () => {
   return (
-    <>
-      <h1>React 19 features</h1>
-    </>
-  )
-}
+    <div className="flex justify-center items-center w-full mt-[10rem]">
+      <Theme />
+    </div>
+  );
+};
 
-export default App
+export default App;
